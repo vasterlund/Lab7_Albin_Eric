d8e4280d86ce919b8e261336043aa1f108990042
Albin Vasterlund <abbe--93@hotmail.com>
2017-10-05 15:15:15 +0200

    sdfsd

---
+++ R/Lab7SpaghettiBolognese.R
@@ -0,0 +1,8 @@
+#'@title Lab7SpaghettiBolognese: Linreg
+#'
+#'@description This package use API from googlemaps to download Longitude and Latitude of a address/destination so you can plot it!
+#'
+#' @name Lab7SpaghettiBolognese
+#' @docType package
+#' @author Albin Vasterlund and Eric Herwin
+NULL
