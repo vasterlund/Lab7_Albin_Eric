e84254958a70eff5771122e71a1b5c279451a50f
Albin Vasterlund <abbe--93@hotmail.com>
2017-10-06 15:09:58 +0200

    hej

--- Albin.R
+++ Albin.R
@@ -1,1 +1,141 @@
-fsdf
+library(MASS)
+
+#####
+data(iris)
+data<-iris
+formula<-Petal.Length~Sepal.Width+Sepal.Length
+x<-model.matrix(formula,data)
+x[,2:ncol(x)]<-apply(x[,2:ncol(x)],2,function(a) (a-mean(a))/sd(a))
+y_namn<-all.vars(formula)[1]
+y<-as.matrix(data[,names(data)==y_namn])
+data<-data.frame(cbind(x[,2:ncol(x)],y))
+names(data)[ncol(data)]<-y_namn
+lm.ridge(formula,data,lambda = seq(0,0.1,0.01))
+#####
+
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,seq(0,0.1,0.01))$print()
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,seq(0,0.1,0.01))$predict()
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,seq(0,0.1,0.01))$coef()
+
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,0.5)$print()
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,0.5)$predict()
+ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,0.5)$coef()
+
+
+
+a<-ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris,c(0.5,0.2))
+Coef<-a$Coef
+lambda<-a$lambda
+Fits<-a$Fits
+
+
+
+#########
+data<-iris
+formula<-Petal.Length~Sepal.Width+Sepal.Length
+lambda<-numeric()
+lambda<-c(0.5,0.2)
+ridgereg<-setRefClass("ridgereg", fields = list(formula="formula", 
+                                                Fits="list",
+                                                Coef="list",
+                                                data="data.frame",
+                                                Call="character",
+                                                lambda="numeric"
+                                            ),
+                    methods = list(
+                      
+                      initialize = function(formula, data, lambda=0){
+                        
+                        
+                        ##### Beräkningar #####
+                        x<-model.matrix(formula,data)
+                        x[,2:ncol(x)]<-apply(x[,2:ncol(x)],2,function(a) (a-mean(a))/sd(a))
+                        y<-all.vars(formula)[1]
+                        y<-as.matrix(data[,names(data)==y])
+                        y_namn<-all.vars(formula)[1]
+                        data1<-data.frame(cbind(x[,2:ncol(x)],y))
+                        names(data1)[ncol(data1)]<-y_namn
+                        data<<-data1
+                        
+                        
+                        fits_lista<-list()
+                        beta_lista<-list()
+                        for ( i in 1:length(lambda)){
+                          
+                          anvanda_lambda<-lambda[i]
+                          
+                          I_lambda<-matrix(nrow=ncol(x),ncol=ncol(x),data = 0)
+                          diag(I_lambda)<-anvanda_lambda
+                          
+                          
+                          b_hat<-(solve((t(x)%*%x)+I_lambda))%*%(t(x)%*%y)
+                          y_fits<-x%*%b_hat
+                          e<-y-y_fits
+                          
+                          coef<-as.numeric(b_hat)
+                          names(coef)<-rownames(b_hat)
+                          
+                          b_hat1<-as.numeric(b_hat)
+                          names(b_hat1)<-rownames(b_hat)
+                          
+                          fits_lista[[i]]<-as.numeric(y_fits)
+                          beta_lista[[i]]<-b_hat1
+                        }
+                        
+                        
+                        
+                        ########### Spara beräkningar ###########
+                        Call1<-character()
+                        Call1[1] <-deparse(substitute(data))
+                        Call1[2] <-Reduce(paste,deparse(formula))
+                        Call1[3] <-deparse(substitute(lambda))
+                        Call<<-paste("linreg(formula = ",Call1[2],", data = ",Call1[1],", lambda=",Call1[3],")",sep="")
+                        
+                        Fits<<-fits_lista
+                        Coef<<-beta_lista
+                        lambda<<-lambda
+                      },
+                      print = function(){
+
+                        beta_avrund<-lapply(Coef, function(a) round(a,5))
+
+                        namn<-c("lambda",names(beta_avrund[[1]]))
+
+                        i=1
+                        j=1
+                        langd_namn<-nchar(namn)
+                        for(i in 1:length(beta_avrund)){
+                          beta_avrund[[i]]<-as.character(c(lambda[i],beta_avrund[[i]]))
+                          for(j in 1:length(namn)){
+                            beta_avrund[[i]][j]<-format(beta_avrund[[i]][j], width=langd_namn[j],justify = c("right"))
+                          }
+
+                        }
+
+                        cat("Call:",sep="\n")
+                        cat(Call, sep="\n")
+                        cat(sep="\n")
+                        cat("Coefficients:",sep="\n")
+                        cat(paste(namn,collapse = "  "),sep="",collapse="\n")
+                        for(i in 1:length(beta_avrund)){
+                          cat(paste(beta_avrund[[i]],collapse = "  "),sep="",collapse="\n")
+                        }
+                      },
+                      predict = function(){
+                        
+                        if(length(Fits)==1){
+                          svar<-Fits[[1]]
+                        }
+                        else svar<-Fits
+                        return(svar)
+                      },
+                      coef = function(){
+                        
+                        if(length(Coef)==1){
+                          svar<-Coef[[1]]
+                        }
+                        else svar<-Coef
+                        return(svar)
+                      }
+                    )
+)
