18304afbd7eb30b94164a88ba2aaa2b6fed05847
Albin Vasterlund <abbe--93@hotmail.com>
2017-10-13 17:52:24 +0200

    dfsd

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -14,9 +14,9 @@
 visualize_airport_delays <- function(){
   
   
-  requireNamespace("nycflights13")
-  requireNamespace("dplyr")
-  requireNamespace("magrittr")
+  # requireNamespace("nycflights13")
+  # requireNamespace("dplyr")
+  # requireNamespace("magrittr")
   
   #requireNamespace("plotly")
   
@@ -28,8 +28,8 @@ visualize_airport_delays <- function(){
   #doin some dplyr stuff
   
   vect <- data %>%
-    group_by(faa) %>%
-    summarize(Mean = mean(arr_delay,na.rm = TRUE))
+    dplyr::group_by(faa) %>%
+    dplyr::summarize(Mean = mean(arr_delay,na.rm = TRUE))
   
   coordin <- data %>%
     group_by(faa) %>%
@@ -50,11 +50,14 @@ visualize_airport_delays <- function(){
   graph_data <- data.frame(vect, coordin[,2],lat,lon)
   
   ###PLOTTA
-  p<-ggplot2::ggplot(graph_data, aes(x=lon, y=lat, color=Mean)) + geom_point(size=3) + 
-    scale_colour_gradient2(low="blue",high="red",mid="grey") +
+  p<-ggplot2::ggplot(graph_data, ggplot2::aes(x=lon, y=lat, color=Mean)) + 
+    ggplot2::geom_point(size=3) + 
+    ggplot2::scale_colour_gradient2(low="blue",high="red",mid="grey") +
     #scale_color_gradient(low="white", high=" black")+
-    theme_bw() + theme(axis.title.y = element_text(angle = 0, hjust = 1))+ 
-    ggtitle("Visualising the delay of the arrival")+ labs(x ="East (Longitude)" , y = "North (Latitude)")
+    ggplot2::theme_bw() + 
+    ggplot2::theme(axis.title.y = ggplot2::element_text(angle = 0, hjust = 1))+ 
+    ggplot2::ggtitle("Visualising the delay of the arrival")+ 
+    ggplot2::labs(x ="East (Longitude)" , y = "North (Latitude)")
   
   # p <- ggplot(graph_data, aes(x = faa, y = Mean, label = Coordinates)) + 
   #   geom_point() + labs(x = "Airports") + theme_bw() +
